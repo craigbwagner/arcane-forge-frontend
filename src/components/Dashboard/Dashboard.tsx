5180b032ac674ae58a359b7f7e2efef044bf08c4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-23 23:02:19 -0400

    add Dashboard template as TS

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  user: {
+    username: string;
+  };
+}
+
+function Dashboard({ user }: Props) {
+  return (
+    <main>
+      <h1>Welcome, {user.username}</h1>
+      <p>
+        This is the dashboard page where you, and only you, can see a dashboard
+        of all of your things.
+      </p>
+    </main>
+  );
+}
+
+export default Dashboard;
