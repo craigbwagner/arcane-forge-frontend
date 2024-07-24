93c77b1674b005855d04eaefaf9e43350463a372
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-23 22:42:32 -0400

    convert existing JS App.jsx template code to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import './App.css'
+import "./App.css";
+import { create } from "zustand";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+import * as authService from "../src/services/authService";
 
-function App() {
-
-  return (
-    <>
-    </>
-  )
+interface State {
+  user: string | null;
+}
+interface Action {
+  updateUser: (user: State["user"]) => void;
 }
 
-export default App
+const useStore = create<State & Action>((set) => ({
+  user: authService.getUser(),
+  updateUser: (user) => set(() => ({ user: user })),
+}));
+
+function App() {
+  const user = useStore((state) => state.user);
+  const updateUser = useStore((state) => state.updateUser);
+
+  function handleSignout() {
+    authService.signout();
+    updateUser(null);
+  }
+  return (
+    <>
+      <h1>Hello</h1>
+      <Navbar user={user} handleSignout={handleSignout} />
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+
+        <Route path="/signup" element={<SignupForm setUser={updateUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={updateUser} />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
