bde537f028eafd809656a024a2b7b37dea82cfdd
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-14 13:16:48 -0400

    update Props interface

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,11 @@
 interface Props {
-  user: {
-    username: string; //might need to add more here
-  };
+  user: string | null;
 }
 
 function Dashboard({ user }: Props) {
   return (
     <main>
-      <h1>Welcome, {user.username}</h1>
+      <h1>Welcome, {user}</h1>
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
         of all of your things.
