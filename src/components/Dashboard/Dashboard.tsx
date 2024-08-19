85e0e4e80489ab6aec64a7d7b16fe250c66ee8c6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-19 15:57:54 -0400

    fix title to display user's username

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ type Props = {
 function Dashboard({ user }: Props) {
   return (
     <main>
-      <h1>Welcome, {user}</h1>
+      <h1>Welcome, {user.username}</h1>
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
         of all of your things.
