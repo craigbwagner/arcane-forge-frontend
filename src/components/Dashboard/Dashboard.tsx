d687b729821bdb6855e3bd12e41e09e2db3dfce2
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-23 18:21:21 -0400

    add left margin so page isn't hidden behind sidebar

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 function Dashboard({ user }: Props) {
   return (
-    <main>
+    <main className="ml-[17rem]">
       <h1>Welcome, {user.username}</h1>
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
