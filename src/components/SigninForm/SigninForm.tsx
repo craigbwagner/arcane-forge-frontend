fdaaa4304074d835346656094c971e6564040bae
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-23 19:18:31 -0400

    convert to zod handled shadcn form

--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -1,70 +1,84 @@
 import * as authService from "../../services/authService";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+const signinSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
 
 function SigninForm(props) {
   const navigate = useNavigate();
-  const [message, setMessage] = useState([""]);
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
+
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
   });
 
-  const updateMessage = (msg) => {
-    setMessage(msg);
-  };
-
-  const handleChange = (e) => {
-    updateMessage("");
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  async function handleSubmit(values: z.infer<typeof signinSchema>, e: any) {
+    // e.preventDefault();
     try {
-      const user = await authService.signin(formData); // TODO build signin service function
-
+      const user = await authService.signin(values);
       props.updateUser(user);
       navigate("/");
     } catch (err) {
-      updateMessage(err.message);
+      console.log(err);
     }
-  };
+  }
 
   return (
     <main className="ml-[17rem]">
       <h1>Log In</h1>
-      <p>{message}</p>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Username:</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="username"
-            value={formData.username}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
             name="username"
-            onChange={handleChange}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={formData.password}
+          <FormField
+            control={form.control}
             name="password"
-            onChange={handleChange}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div>
-          <button>Log In</button>
-          <Link to="/">
-            <button>Cancel</button>
-          </Link>
-        </div>
-      </form>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
   );
 }
