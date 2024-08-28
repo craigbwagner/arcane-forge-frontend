b91ecaa06867c177b53e149fed27929d7b28cf33
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-28 19:05:08 -0400

    add Character interface to state as well as empty Ability, Item, and Class interfaces

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,48 @@
 import { produce } from "immer";
 import { create } from "zustand";
 
+interface Ability {}
+
+interface Item {}
+
+interface Class {}
+
 interface State {
   readonly user: { username: string; _id: string } | null;
+  readonly character: {
+    creator: string;
+    name?: string;
+    race?: string;
+    classes?: Class[];
+    level?: number;
+    sex?: string;
+    size?: string;
+    age?: number;
+    height?: string;
+    weight?: number;
+    alignment?: string;
+    languages?: string[];
+    initiative?: number;
+    initiativeMods?: number;
+    speed?: number;
+    maxHP?: number;
+    currentHP?: number;
+    tempHP?: number;
+    hitDiceRemaining?: number;
+    hitDiceType?: string;
+    hitDiceTotal?: number;
+    strength?: number;
+    dexterity?: number;
+    constitution?: number;
+    intelligence?: number;
+    wisdom?: number;
+    charisma?: number;
+    savingThrowProficiencies?: string[];
+    skillProficiencies?: string[];
+    skillExpertise?: string[];
+    abilities?: Ability[];
+    items?: Item[];
+  };
   readonly updateUser: (
     updatedUser: { username: string; _id: string } | null,
   ) => void;
@@ -16,6 +56,7 @@ const useStore = create<State>()((set) => ({
         draft.user = updatedUser;
       }),
     ),
+  character: {},
 }));
 
 export default useStore;
