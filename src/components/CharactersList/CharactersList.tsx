40866b76d8ba234b5dda56e4083b63c4f0287864
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-29 16:06:26 -0400

    create add character button

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -1,14 +1,37 @@
 import useStore from "@/store/store";
+import { Button } from "../ui/button";
+import * as charactersService from "../../services/characterService";
+import { Form, useNavigate } from "react-router-dom";
 
 function CharactersList() {
-  const characters = useStore((state) => state.user.characters);
+  const navigate = useNavigate();
+  const user = useStore((state) => state.user);
+  const addUserCharacter = useStore((state) => state.addUserCharacter);
+  if (!user) {
+    throw new Error("No current user.");
+  }
+  const characters = user.characters;
+
+  async function handleSubmit(e: any): Promise<void> {
+    e.preventDefault();
+    try {
+      const newCharacter = await charactersService.create(user._id);
+      addUserCharacter(newCharacter);
+      console.log(user);
+
+      navigate(`/characters/${newCharacter._id}`);
+    } catch (err: unknown) {
+      console.log(err);
+    }
+  }
+
   return (
-    <>
+    <main className="ml-[17rem]">
       <h1>My Characters</h1>
-      {characters.map((character) => {
-        <li>character</li>;
-      })}
-    </>
+      <form onSubmit={handleSubmit}>
+        <Button type="submit">Add Character</Button>
+      </form>
+    </main>
   );
 }
 
