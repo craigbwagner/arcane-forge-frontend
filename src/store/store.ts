39ff82001774c4c8d165a411702c50e161cb0d26
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-28 22:14:16 -0400

    add character and updateCharacter() to state

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,56 +7,65 @@ interface Item {}
 
 interface Class {}
 
+interface Character {
+  creator: string;
+  name?: string;
+  race?: string;
+  classes?: Class[];
+  level?: number;
+  sex?: string;
+  size?: string;
+  age?: number;
+  height?: string;
+  weight?: number;
+  alignment?: string;
+  languages?: string[];
+  initiative?: number;
+  initiativeMods?: number;
+  speed?: number;
+  maxHP?: number;
+  currentHP?: number;
+  tempHP?: number;
+  hitDiceRemaining?: number;
+  hitDiceType?: string;
+  hitDiceTotal?: number;
+  strength?: number;
+  dexterity?: number;
+  constitution?: number;
+  intelligence?: number;
+  wisdom?: number;
+  charisma?: number;
+  savingThrowProficiencies?: string[];
+  skillProficiencies?: string[];
+  skillExpertise?: string[];
+  abilities?: Ability[];
+  items?: Item[];
+}
+
 interface State {
   readonly user: { username: string; _id: string } | null;
-  readonly character: {
-    creator: string;
-    name?: string;
-    race?: string;
-    classes?: Class[];
-    level?: number;
-    sex?: string;
-    size?: string;
-    age?: number;
-    height?: string;
-    weight?: number;
-    alignment?: string;
-    languages?: string[];
-    initiative?: number;
-    initiativeMods?: number;
-    speed?: number;
-    maxHP?: number;
-    currentHP?: number;
-    tempHP?: number;
-    hitDiceRemaining?: number;
-    hitDiceType?: string;
-    hitDiceTotal?: number;
-    strength?: number;
-    dexterity?: number;
-    constitution?: number;
-    intelligence?: number;
-    wisdom?: number;
-    charisma?: number;
-    savingThrowProficiencies?: string[];
-    skillProficiencies?: string[];
-    skillExpertise?: string[];
-    abilities?: Ability[];
-    items?: Item[];
-  };
+  readonly character: Character | null;
   readonly updateUser: (
     updatedUser: { username: string; _id: string } | null,
   ) => void;
+  readonly updateCharacter: (updatedCharacter: Character | null) => void;
 }
 
 const useStore = create<State>()((set) => ({
   user: { username: "", _id: "" },
+  character: null,
   updateUser: (updatedUser) =>
     set(
       produce((draft) => {
         draft.user = updatedUser;
       }),
     ),
-  character: {},
+  updateCharacter: (updatedCharacter) =>
+    set(
+      produce((draft) => {
+        draft.character = updatedCharacter;
+      }),
+    ),
 }));
 
 export default useStore;
