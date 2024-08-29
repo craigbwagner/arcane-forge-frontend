4a644032207c72443b1bf6bc7e9d20e9f7ebe695
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-29 16:05:57 -0400

    add authorization header to POST

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -1,16 +1,23 @@
+import useStore, { Character } from "../store/store";
+
 const BACKEND_URL: string = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 
-async function create(creator: string) {
+async function create(creator: string): Promise<typeof Character> {
   try {
     const res = await fetch(`${BACKEND_URL}/characters/new`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
       body: JSON.stringify({ creator }),
     });
     const json = await res.json();
+    json.creator = creator;
     if (json.err) {
       throw new Error(json.err);
     }
+    return json;
   } catch (err: unknown) {
     console.log(err);
     throw err;
