77f910acb7eaf27cf73a9d5d2c166e6d97e5bf56
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-30 11:50:35 -0400

    add zod characterSchema

--- src/components/CharacterSheet/CharacterSheet.tsx
+++ src/components/CharacterSheet/CharacterSheet.tsx
@@ -1,4 +1,40 @@
 import useStore from "../../store/store";
+import { number, string, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+const characterSchema = z.object({
+  name: z.string(),
+  race: z.string(),
+  classes: z.string().array(),
+  sex: string(),
+  size: string(),
+  age: number(),
+  height: string(),
+  weight: number(),
+  alignment: string(),
+  languages: string().array(),
+  initiative: number(),
+  speed: number(),
+  maxHP: number(),
+  currentHP: number(),
+  tempHP: number(),
+  hitDiceRemaining: number(),
+  hitDiceType: string(),
+  hitDiceTotal: number(),
+  skillExpertise: string().array(),
+});
 
 function CharacterSheet() {
   const currentCharacter = useStore((state) => state.currentCharacter);
