a5f16702e98193ba81f9df8ad16fccde35a11859
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-01 16:50:02 -0400

    add test property  in vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
@@ -9,5 +11,15 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
   },
 });
