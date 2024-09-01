761f3828484191e6b3a0660ca46316990ae1fa91
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-01 19:47:02 -0400

    add zusty for zustand devtool

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { produce } from "immer";
 import { create } from "zustand";
+import zustymiddlewarets from "zustymiddlewarets";
 
 interface Ability {}
 
@@ -61,59 +62,68 @@ interface State {
   addUserCharacter: (newCharacter: Character) => void;
 }
 
-const useStore = create<State>()((set) => ({
-  user: { username: "", _id: "", characters: [] },
-  currentCharacter: {
-    creator: "",
-    name: "",
-    race: "",
-    classes: [],
-    level: 1,
-    sex: "",
-    size: "",
-    age: 0,
-    height: "",
-    weight: 0,
-    alignment: "",
-    languages: [],
-    initiative: 0,
-    speed: 0,
-    maxHP: 0,
-    currentHP: 0,
-    tempHP: 0,
-    hitDiceRemaining: 0,
-    hitDiceType: "",
-    hitDiceTotal: 1,
-    strength: 0,
-    dexterity: 0,
-    constitution: 0,
-    intelligence: 0,
-    wisdom: 0,
-    charisma: 0,
-    savingThrowProficiencies: [],
-    skillProficiencies: [],
-    skillExpertise: [],
-    abilities: [],
-    items: [],
-  },
-  updateUser: (updatedUser) =>
-    set(
-      produce((draft) => {
-        draft.user = updatedUser;
-      }),
-    ),
-  updateCharacter: (updatedCharacter) =>
-    set(
-      produce((draft: State) => {
-        draft.currentCharacter = updatedCharacter;
-      }),
-    ),
-  addUserCharacter: (newCharacter) =>
-    set(
-      produce((draft: State) => {
-        if (draft.user) draft.user.characters.push(newCharacter);
-      }),
-    ),
-}));
+const useStore = create<State>(
+  zustymiddlewarets((set) => ({
+    user: null,
+    currentCharacter: {
+      creator: "",
+      name: "",
+      race: "",
+      classes: [],
+      level: 1,
+      sex: "",
+      size: "",
+      age: 0,
+      height: "",
+      weight: 0,
+      alignment: "",
+      languages: [],
+      initiative: 0,
+      speed: 0,
+      maxHP: 0,
+      currentHP: 0,
+      tempHP: 0,
+      hitDiceRemaining: 0,
+      hitDiceType: "",
+      hitDiceTotal: 1,
+      strength: 0,
+      dexterity: 0,
+      constitution: 0,
+      intelligence: 0,
+      wisdom: 0,
+      charisma: 0,
+      savingThrowProficiencies: [],
+      skillProficiencies: [],
+      skillExpertise: [],
+      abilities: [],
+      items: [],
+    },
+    updateUser: (updatedUser) =>
+      set(
+        produce((draft) => {
+          draft.user = updatedUser;
+        }),
+      ),
+    updateCharacter: (updatedCharacter) =>
+      set(
+        produce((draft: State) => {
+          draft.currentCharacter = updatedCharacter;
+        }),
+      ),
+    addUserCharacter: (newCharacter) =>
+      set(
+        produce((draft: State) => {
+          if (draft.user) draft.user.characters.push(newCharacter);
+        }),
+      ),
+  })),
+);
 
+declare global {
+  interface Window {
+    store: typeof useStore;
+  }
+}
+
+window.store = useStore;
 export default useStore;
