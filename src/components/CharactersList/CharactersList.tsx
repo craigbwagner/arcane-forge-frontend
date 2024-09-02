5af2beda005d3d568ff7b2425e457a1f3eff9045
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-01 20:48:09 -0400

    add Cards for characters

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -1,8 +1,42 @@
 import useStore from "../../store/store";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function CharactersList() {
   const characters = useStore((state) => state.user?.characters);
-  return <div>CharactersList</div>;
+  return (
+    <main className="ml-[17rem">
+      <Card></Card>
+      {characters ? (
+        <ul>
+          {characters.map((character) => {
+            return (
+              <Card>
+                <CardHeader>
+                  <CardTitle>{character.name}</CardTitle>
+                  <CardDescription>
+                    Level {character.level} character
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Card Content</p>
+                </CardContent>
+                <CardFooter></CardFooter>
+              </Card>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No characters created.</p>
+      )}
+    </main>
+  );
 }
 
 export default CharactersList;
