362ca71b8e37df8d28321351196271e961ccc147
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-02 17:57:57 -0400

    update page to flex columns

--- src/pages/CharacterSheet.tsx
+++ src/pages/CharacterSheet.tsx
@@ -160,8 +160,7 @@ function CharacterSheet() {
   }
 
   return (
-    <main className="ml-[17rem]">
-      <h1>Current Character</h1>
+    <main className="flex flex-col ml-[17rem]">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(saveCharacter)}>
           <Card>
@@ -309,6 +308,148 @@ function CharacterSheet() {
             </CardContent>
             <CardFooter></CardFooter>
           </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Base Stats</CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent className="grid grid-cols-3 gap-2">
+              <div>
+                <FormField
+                  control={form.control}
+                  name="initiative"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Initiative</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Initiative"
+                          type="number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="speed"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Speed</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Speed" type="number" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="maxHP"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Max HP</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Max HP" type="number" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="currentHP"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Current HP</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Current HP"
+                          type="number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="tempHP"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Temp HP</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Temp HP" type="number" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="hitDiceRemaining"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Remaining Hit Die</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Remaining Hit Die"
+                          type="number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="hitDiceType"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Hit Dice Type</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Hit Dice Type" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="hitDiceTotal"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Max # of Hit Die</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Total Hit Die"
+                          type="number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
           <FormField
             control={form.control}
             name="languages"
@@ -317,122 +458,6 @@ function CharacterSheet() {
                 <FormLabel>Languages</FormLabel>
                 <FormControl>
                   <Input placeholder="Languages" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="initiative"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Initiative</FormLabel>
-                <FormControl>
-                  <Input placeholder="Initiative" type="number" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="speed"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Speed</FormLabel>
-                <FormControl>
-                  <Input placeholder="Speed" type="number" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="maxHP"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Max HP</FormLabel>
-                <FormControl>
-                  <Input placeholder="Max HP" type="number" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="currentHP"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Current HP</FormLabel>
-                <FormControl>
-                  <Input placeholder="Current HP" type="number" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="tempHP"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Temp HP</FormLabel>
-                <FormControl>
-                  <Input placeholder="Temp HP" type="number" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="hitDiceRemaining"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Remaining Hit Die</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Remaining Hit Die"
-                    type="number"
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="hitDiceType"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Hit Dice Type</FormLabel>
-                <FormControl>
-                  <Input placeholder="Hit Dice Type" {...field} />
-                </FormControl>
-                <FormDescription></FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="hitDiceTotal"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Max # of Hit Die</FormLabel>
-                <FormControl>
-                  <Input placeholder="Total Hit Die" type="number" {...field} />
                 </FormControl>
                 <FormDescription></FormDescription>
                 <FormMessage />
