4b3ec8a376923dbafa77c8e6b9c6e389205fca3b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-04 17:52:30 -0400

    add input fields

---
+++ src/components/CharacterSheet/CharBasicDetailsForm.tsx
@@ -0,0 +1,193 @@
+import useStore from "@/store/store";
+import { UseFormReturn } from "react-hook-form";
+import {
+  FormField,
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+interface FormData {
+  name: string;
+  race: string;
+  sex: string;
+  size: string;
+  age: number;
+  height: string;
+  weight: number;
+  alignment: string;
+  languages: string[];
+  initiative: number;
+  speed: number;
+  maxHP: number;
+  currentHP: number;
+  tempHP: number;
+  hitDiceRemaining: number;
+  hitDiceType: string;
+  hitDiceTotal: number;
+}
+
+function CharBasicDetailsForm({
+  form,
+}: {
+  form: UseFormReturn<FormData, any, undefined>;
+}) {
+  const currentCharacter = useStore((state) => state.currentCharacter);
+  return (
+    <div className="grid grid-cols-3">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Name" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="race"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Race</FormLabel>
+            <FormControl>
+              <Input placeholder="Race" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="sex"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Gender</FormLabel>
+            <FormControl>
+              <Input placeholder="Gender" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="size"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Size</FormLabel>
+            <FormControl>
+              <Input placeholder="Size" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="age"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Age</FormLabel>
+            <FormControl>
+              <Input placeholder="Age" type="number" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="height"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Height</FormLabel>
+            <FormControl>
+              <Input placeholder="Height" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="weight"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Weight</FormLabel>
+            <FormControl>
+              <Input placeholder="Weight" type="number" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="alignment"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Alignment</FormLabel>
+            <FormControl>
+              <Input placeholder="Alignment" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <div>
+        <h1>Level</h1>
+        <h2>{currentCharacter.level}</h2>
+      </div>
+      <div>
+        <h1>Name</h1>
+        <h2>{currentCharacter.name}</h2>
+      </div>
+      <div>
+        <h1>Race</h1>
+        <h2>{currentCharacter.race}</h2>
+      </div>
+      <div>
+        <h1>Sex</h1>
+        <h2>{currentCharacter.sex}</h2>
+      </div>
+      <div>
+        <h1>Size</h1>
+        <h2>{currentCharacter.size}</h2>
+      </div>
+      <div>
+        <h1>Age</h1>
+        <h2>{currentCharacter.age}</h2>
+      </div>
+      <div>
+        <h1>Height</h1>
+        <h2>{currentCharacter.height}</h2>
+      </div>
+      <div>
+        <h1>Weight</h1>
+        <h2>{currentCharacter.weight}</h2>
+      </div>
+      <div>
+        <h1>Alignment</h1>
+        <h2>{currentCharacter.alignment}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default CharBasicDetailsForm;
