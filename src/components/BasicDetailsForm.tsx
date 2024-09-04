cd0d7c074edd1d099983163f8698098da7bd0140
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-03 20:18:02 -0400

    move basic details section to component

---
+++ src/components/BasicDetailsForm.tsx
@@ -0,0 +1,131 @@
+import { UseFormReturn } from "react-hook-form";
+import {
+  FormField,
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+function BasicDetailsForm({ form }: { form: UseFormReturn }) {
+  return (
+    <div className="grid grid-cols-3">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Name" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="race"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Race</FormLabel>
+            <FormControl>
+              <Input placeholder="Race" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="sex"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Sex</FormLabel>
+            <FormControl>
+              <Input placeholder="Sex" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="size"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Size</FormLabel>
+            <FormControl>
+              <Input placeholder="Size" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="age"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Age</FormLabel>
+            <FormControl>
+              <Input placeholder="Age" type="number" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="height"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Height</FormLabel>
+            <FormControl>
+              <Input placeholder="Height" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="weight"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Weight</FormLabel>
+            <FormControl>
+              <Input placeholder="Weight" type="number" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="alignment"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Alignment</FormLabel>
+            <FormControl>
+              <Input placeholder="Alignment" {...field} />
+            </FormControl>
+            <FormDescription></FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
+
+export default BasicDetailsForm;
--- src/pages/CharacterSheet.tsx
+++ src/pages/CharacterSheet.tsx
@@ -24,6 +24,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import BasicDetailsForm from "@/components/BasicDetailsForm";
 
 const characterSchema = z.object({
   name: z.string(),
@@ -199,119 +200,8 @@ function CharacterSheet() {
               <CardTitle>Basic Details</CardTitle>
               <CardDescription></CardDescription>
             </CardHeader>
-            <CardContent className="grid grid-cols-3">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Name" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="race"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Race</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Race" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="sex"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Sex</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Sex" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="size"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Size</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Size" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="age"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Age</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Age" type="number" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="height"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Height</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Height" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="weight"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Weight</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Weight" type="number" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="alignment"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Alignment</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Alignment" {...field} />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <CardContent>
+              <BasicDetailsForm form={form} />
             </CardContent>
           </Card>
 
