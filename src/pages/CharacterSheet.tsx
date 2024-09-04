471cf5c38848a4fe4b7b7b8fd46ecbb120615aa0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-04 13:36:54 -0400

    remove Input shadcn component import

--- src/pages/CharacterSheet.tsx
+++ src/pages/CharacterSheet.tsx
@@ -7,7 +7,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../components/ui/button";
 import { Form } from "../components/ui/form";
-import { Input } from "../components/ui/input";
 import {
   Card,
   CardContent,
