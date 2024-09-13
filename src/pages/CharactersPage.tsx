9da02d1a85233036d22569336b68b088b490a8a1
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-13 17:04:17 -0400

    rename initializeCharacterValues to initialCharacterValues and add skills to initial values

--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -10,7 +10,7 @@ function CharactersPage() {
   const addUserCharacter = useStore((state) => state.addUserCharacter);
   const updateCharacter = useStore((state) => state.updateCharacter);
 
-  const initializeCharacterValues = {
+  const initialCharacterValues = {
     creator: user?._id ?? "",
     name: "",
     race: "",
@@ -21,7 +21,7 @@ function CharactersPage() {
     age: 0,
     height: "",
     weight: 0,
-    alignment: "",
+    alignment: "" as const,
     languages: [],
     initiative: 0,
     speed: 0,
@@ -38,8 +38,134 @@ function CharactersPage() {
     wisdom: 0,
     charisma: 0,
     savingThrowProficiencies: [],
-    skillProficiencies: [],
-    skillExpertise: [],
+    skills: [
+      {
+        name: "Acrobatics",
+        attributeName: "acrobatics",
+        ability: "DEX",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Animal Handling",
+        attributeName: "animal-handling",
+        ability: "WIS",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Arcana",
+        attributeName: "arcana",
+        ability: "INT",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Athletics",
+        attributeName: "athletics",
+        ability: "STR",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Deception",
+        attributeName: "deception",
+        ability: "CHA",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "History",
+        attributeName: "history",
+        ability: "INT",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Insight",
+        attributeName: "insight",
+        ability: "WIS",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Intimidation",
+        attributeName: "intimidation",
+        ability: "CHA",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Investigation",
+        attributeName: "investigation",
+        ability: "INT",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Medicine",
+        attributeName: "medicine",
+        ability: "WIS",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Nature",
+        attributeName: "nature",
+        ability: "INT",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Perception",
+        attributeName: "perception",
+        ability: "WIS",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Performance",
+        attributeName: "performance",
+        ability: "CHA",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Persuasion",
+        attributeName: "persuasion",
+        ability: "CHA",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Religion",
+        attributeName: "religion",
+        ability: "INT",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Sleight of Hand",
+        attributeName: "sleight-of-hand",
+        ability: "DEX",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Stealth",
+        attributeName: "stealth",
+        ability: "DEX",
+        isProficient: false,
+        hasExpertise: false,
+      },
+      {
+        name: "Survival",
+        attributeName: "survival",
+        ability: "WIS",
+        isProficient: false,
+        hasExpertise: false,
+      },
+    ],
     abilities: [],
     items: [],
   };
@@ -49,7 +175,7 @@ function CharactersPage() {
     try {
       if (user) {
         const newCharacter: Character = await charactersService.create(
-          initializeCharacterValues,
+          initialCharacterValues,
         );
         addUserCharacter(newCharacter);
         updateCharacter(newCharacter);
