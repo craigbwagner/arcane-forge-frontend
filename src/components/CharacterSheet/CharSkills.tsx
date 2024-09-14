501652f44b9c48820725b31c6f3031e4582676ed
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-14 18:38:04 -0400

    update to receive skills data from state instead of calculating based on abilities array

--- src/components/CharacterSheet/CharSkills.tsx
+++ src/components/CharacterSheet/CharSkills.tsx
@@ -1,52 +1,9 @@
 import useStore, { Character } from "@/store/store";
 
-interface Ability {
-  name: string;
-  abilityScore: number;
-  abilityMod: number;
-  proficientSave: boolean;
-  savingThrowMod: number;
-}
-type Abilities = Ability[];
-
-function CharSkills({ abilities }: { abilities: Abilities }) {
+function CharSkills() {
   const currentCharacter = useStore(
     (state) => state.currentCharacter,
   ) as Character;
-
-  currentCharacter.skills.forEach((skill) => {
-    let skillMod: SkillMod = { skillName: skill.name, modValue: 0 };
-    switch (skill.ability) {
-      case "STR":
-        skillMod.modValue = abilities[0].abilityMod;
-        break;
-      case "DEX":
-        skillMod.modValue = abilities[1].abilityMod;
-        break;
-      case "CON":
-        skillMod.modValue = abilities[2].abilityMod;
-        break;
-      case "CHA":
-        skillMod.modValue = abilities[3].abilityMod;
-        break;
-      case "WIS":
-        skillMod.modValue = abilities[4].abilityMod;
-        break;
-      case "INT":
-        skillMod.modValue = abilities[5].abilityMod;
-        break;
-    }
-
-    if (skill.isProficient) {
-      if (skill.hasExpertise) {
-        skillMod.modValue += (currentCharacter.proficiencyBonus as number) * 2;
-        skillMods.push(skillMod);
-      } else {
-        skillMod.modValue += currentCharacter.proficiencyBonus as number;
-        skillMods.push(skillMod);
-      }
-    }
-  });
 
   return (
     <ul>
