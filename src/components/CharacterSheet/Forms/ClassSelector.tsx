4356d9df3fc5a4b97c75e2e8d9c0fcb4d28864da
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-21 15:30:49 -0400

    add select input for class and number input for level

--- src/components/CharacterSheet/Forms/ClassSelector.tsx
+++ src/components/CharacterSheet/Forms/ClassSelector.tsx
@@ -21,11 +21,38 @@ import { FormData } from "@/pages/EditCharacterPage";
 function ClassSelector({
   characterClass,
   form,
+  index,
 }: {
   characterClass: CharacterClass;
   form: UseFormReturn<FormData, any, undefined>;
+  index: number;
 }) {
-  return <div>ClassSelector</div>;
+  return (
+    <div className="flex">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Class" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Artificer">Artificer</SelectItem>
+          <SelectItem value="Barbarian">Barbarian</SelectItem>
+          <SelectItem value="Bard">Bard</SelectItem>
+          <SelectItem value="Blood Hunter">Blood Hunter</SelectItem>
+          <SelectItem value="Cleric">Cleric</SelectItem>
+          <SelectItem value="Druid">Druid</SelectItem>
+          <SelectItem value="Fighter">Fighter</SelectItem>
+          <SelectItem value="Monk">Monk</SelectItem>
+          <SelectItem value="Paladin">Paladin</SelectItem>
+          <SelectItem value="Ranger">Ranger</SelectItem>
+          <SelectItem value="Rogue">Rogue</SelectItem>
+          <SelectItem value="Sorcerer">Sorcerer</SelectItem>
+          <SelectItem value="Warlock">Warlock</SelectItem>
+          <SelectItem value="Wizard">Wizard</SelectItem>
+        </SelectContent>
+      </Select>
+      <Input type="number"></Input>
+    </div>
+  );
 }
 
 export default ClassSelector;
