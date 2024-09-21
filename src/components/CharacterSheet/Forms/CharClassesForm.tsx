48a1d7cca0c48b2803e25b99d4b9a0b8969e18c8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-21 14:33:56 -0400

    add button to add new class and map classes onto ClassSelector components

--- src/components/CharacterSheet/Forms/CharClassesForm.tsx
+++ src/components/CharacterSheet/Forms/CharClassesForm.tsx
@@ -1,13 +1,23 @@
 import useStore from "@/store/store";
 import { UseFormReturn } from "react-hook-form";
 import { FormData } from "@/pages/EditCharacterPage";
+import { Button } from "../../ui/button";
+import ClassSelector from "./ClassSelector";
 
 function CharClassesForm({
   form,
 }: {
   form: UseFormReturn<FormData, any, undefined>;
 }) {
-  return <div>CharClassesForm</div>;
+  const currentCharacter = useStore((state) => state.currentCharacter);
+  return (
+    <div>
+      {currentCharacter.classes.map((characterClass) => {
+        <ClassSelector characterClass={characterClass} form={form} />;
+      })}
+      <Button>Add Class</Button>
+    </div>
+  );
 }
 
 export default CharClassesForm;
