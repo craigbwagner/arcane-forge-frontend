f96990b2c54c437ff130d8cd3b968112a910add7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-21 14:32:34 -0400

    remove form field component imports

--- src/components/CharacterSheet/Forms/CharClassesForm.tsx
+++ src/components/CharacterSheet/Forms/CharClassesForm.tsx
@@ -1,21 +1,5 @@
 import useStore from "@/store/store";
 import { UseFormReturn } from "react-hook-form";
-import {
-  FormField,
-  FormControl,
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../../ui/form";
-import { Input } from "../../ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { FormData } from "@/pages/EditCharacterPage";
 
 function CharClassesForm({
