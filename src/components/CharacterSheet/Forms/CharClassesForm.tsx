fac73d76a46568a81e287c97b9dc9b54643b4e72
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-24 19:37:54 -0400

    delete ClassSelector component and add state for new class inputs

--- src/components/CharacterSheet/Forms/CharClassesForm.tsx
+++ src/components/CharacterSheet/Forms/CharClassesForm.tsx
@@ -1,7 +1,16 @@
 import useStore from "@/store/store";
+import { useState } from "react";
 import { UseFormReturn } from "react-hook-form";
 import { FormData } from "@/pages/EditCharacterPage";
 import { Button } from "../../ui/button";
+import { Input } from "../../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import ClassSelector from "./ClassSelector";
 
 function CharClassesForm({
@@ -11,10 +20,17 @@ function CharClassesForm({
 }) {
   const currentCharacter = useStore((state) => state.currentCharacter);
   const updateCharacter = useStore((state) => state.updateCharacter);
+  const [newClass, setNewClass] = useState("Class");
+  const [newSubclass, setNewSubclass] = useState("");
+  const [newLevel, setNewLevel] = useState(1);
 
   function handleAddClass() {
-    const newClass = { name: "", subclass: "", level: 1 };
-    const updatedClasses = [...currentCharacter.classes, newClass];
+    const addedClass = {
+      name: newClass,
+      subclass: newSubclass,
+      level: newLevel,
+    };
+    const updatedClasses = [...currentCharacter.classes, addedClass];
 
     updateCharacter({ ...currentCharacter, classes: updatedClasses });
   }
@@ -28,7 +44,34 @@ function CharClassesForm({
           index={index}
         />
       ))}
-      <Button onClick={handleAddClass}>Add Class</Button>
+      <div className="flex">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Class" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Artificer">Artificer</SelectItem>
+            <SelectItem value="Barbarian">Barbarian</SelectItem>
+            <SelectItem value="Bard">Bard</SelectItem>
+            <SelectItem value="Blood Hunter">Blood Hunter</SelectItem>
+            <SelectItem value="Cleric">Cleric</SelectItem>
+            <SelectItem value="Druid">Druid</SelectItem>
+            <SelectItem value="Fighter">Fighter</SelectItem>
+            <SelectItem value="Monk">Monk</SelectItem>
+            <SelectItem value="Paladin">Paladin</SelectItem>
+            <SelectItem value="Ranger">Ranger</SelectItem>
+            <SelectItem value="Rogue">Rogue</SelectItem>
+            <SelectItem value="Sorcerer">Sorcerer</SelectItem>
+            <SelectItem value="Warlock">Warlock</SelectItem>
+            <SelectItem value="Wizard">Wizard</SelectItem>
+          </SelectContent>
+        </Select>
+        <label htmlFor="level">Level</label>
+        <Input type="number" name="level" value={newLevel}></Input>
+        <label htmlFor="subclass">Subclass</label>
+        <Input name="subclass"></Input>
+        <Button onClick={handleAddClass}>Add Class</Button>
+      </div>
     </div>
   );
 }
