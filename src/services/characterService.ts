cc6cbc3f63222ec6a1f3a219cd50e86b71308226
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-25 14:02:52 -0400

    add deleteCharacter service function

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -64,4 +64,24 @@ async function saveCharacter(characterData: any) {
   }
 }
 
-export { create, getCharacter, saveCharacter };
+async function deleteCharacter(characterId: string) {
+  try {
+    const res = await fetch(`${BACKEND_URL}/characters/${characterId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const json = await res.json();
+    if (json.err) {
+      throw new Error(json.err);
+    }
+    return json;
+  } catch (err: unknown) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export { create, getCharacter, saveCharacter, deleteCharacter };
